e35321d1b5f2efd2b15d0ebbcc34990612319454
Innoman <innoman.noma@gmail.com>
2020-11-03 12:59:03 +0100

    W

---
+++ fronted/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id:"1",
+            name:"iphone 6",
+            category:"iPhone",
+            image:"/images/iphone3.jpg",
+            price:3999,
+            brand:"Apple",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality iphone"
+        },
+        {
+            _id:"2",
+            name:"iphone 7",
+            category:"iPhone",
+            image:"/images/iphone3.jpg",
+            price:4999,
+            brand:"Apple",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality iphone"
+        },
+        {
+            _id:"3",
+            name:"iphone 9",
+            category:"iphone",
+            image:"/images/iphone3.jpg",
+            price:7999,
+            brand:"Apple",
+            rating:4.5,
+            numReviews:9,
+            description:"High quality iphone"
+        },
+        {
+            _id:"4",
+            name:"iphone X",
+            category:"iPhone",
+            image:"/images/iphone3.jpg",
+            price:3999,
+            brand:"Apple",
+            rating:4.5,
+            numReviews:8,
+            description:"High quality iphone"
+        },
+        {
+            _id:"5",
+            name:"iphone 8",
+            category:"iPhone",
+            image:"/images/iphone3.jpg",
+            price:3999,
+            brand:"Apple",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality iphone"
+        },
+        {
+            _id:"6",
+            name:"MacBook 11 tum",
+            category:"MacBook",
+            image:"/images/macbook1.jpg",
+            price:12000,
+            brand:"Apple",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality MacBook"
+        }
+    ]
+};
+export default data
